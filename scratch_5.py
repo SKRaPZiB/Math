16e2ce8b6dca9a5cea51dbb2f3384285900fb3f7
SKRaPZiB <70914000+SKRaPZiB@users.noreply.github.com>
2021-12-12 18:41:37 +0300

    Add files via upload

---
+++ scratch_5.py
@@ -0,0 +1,61 @@
+import numpy as np
+
+
+class Interpolation:
+    @staticmethod
+    def polynomial_interpolation(x, y, xl):
+        yl = []
+        for xl_value in xl:
+            # Полином
+            L = 0
+            # Во вложенных циклах i == j т.к x и y имеют равную длинну
+            for j in range(len(x)):
+                # Промежуточные значения полинома. Первоначально предполагаем, что они равны y[i]
+                intermediate_polynom_value1 = y[j]
+                intermediate_polynom_value2 = y[j]
+
+                for i in range(len(x)):
+                    # При i == j pl2 обращается в 0
+                    if i == j:
+                        continue
+
+                    # Находим числитель и знаменитель для нашего полинома и умножаем все на y[n]
+                    intermediate_polynom_value1 = (
+                                                          xl_value - x[i]) * intermediate_polynom_value1
+                    intermediate_polynom_value2 = (
+                                                          x[j] - x[i]) * intermediate_polynom_value2
+                # Находим значение L[i]
+                L = L + (y[j] * (intermediate_polynom_value1 /
+                                 intermediate_polynom_value2))
+
+            # Добавляем полученное значение полинома для xl_value. Полученный массив вернём из функции как значения по Y
+            yl.append(L)
+
+        return yl
+
+    @staticmethod
+    def piecewise_linear_interpolation(x_values, y_values, xl):
+        res = 0
+        for i in range(len(x_values)):
+            if x_values[i - 1] <= xl <= x_values[i]:
+                x = xl - x_values[i]
+                yp = y_values[i] - y_values[i - 1]
+                xp = x_values[i] - x_values[i - 1]
+                res = y_values[i] + ((yp / xp) * x)
+                break
+        return res
+
+    @staticmethod
+    def piecewise_parabolic_interpolation(x, y, arg):
+        res = 0
+        for i in range(len(x) - 1):
+            if arg >= x[i] and arg <= x[i + 1]:
+                matrix = np.array(
+                    [[x[i - 1] ** 2, x[i - 1], 1],
+                     [x[i] ** 2, x[i], 1],
+                     [x[i + 1] ** 2, x[i + 1], 1]])
+                vector = np.array([y[i - 1], y[i], y[i + 1]])
+                poly = np.linalg.solve(matrix, vector)
+                res = poly[0] * arg ** 2 + poly[1] * arg + poly[2]
+
+        return res
---
+++ scratch_8.py
@@ -0,0 +1,88 @@
+import matplotlib.pyplot as plt
+import parser
+
+import numpy as np
+from scipy import interpolate
+
+from inputs.filter_data import filter_data
+from interpolation import Interpolation
+from inputs.parse_interpolation_mode import parse_interpolation_mode
+from approximation import approximation
+
+
+class Plotter:
+    def __init__(self, data_path) -> None:
+        self.data = parser.parse_json_data(data_path)
+
+    def _plot(self, x, y, label):
+        plt.scatter(x, y, label=label)
+
+    def _setting_and_show(self, title):
+        plt.xlabel("Значение по X")
+        plt.ylabel("Значение по Y")
+        plt.title(title)
+        plt.legend()
+        plt.show()
+
+    def plot_graph(self):
+        for label, cords in filter_data(self.data).items():
+            self._plot(cords['x'], cords['y'], label)
+        self._setting_and_show("Простой поточечный график")
+
+    def plot_interpolation(self):
+        mode = parse_interpolation_mode()
+
+        if mode == '1':
+            self.draw_polynominal_interpolation()
+        if mode == '2':
+            self.draw_piecewise_linear_interpolation()
+        if mode == '3':
+            self.draw_piecewise_parabolic_interpolation()
+        if mode == '4':
+            self.draw_spline_interpolation()
+
+    def draw_polynominal_interpolation(self):
+        filtered_data = filter_data(self.data)
+        for label, cords in filtered_data.items():
+            xl = np.linspace(min(cords['x']), max(cords['x']))
+            yl = Interpolation.polynomial_interpolation(cords['x'], cords['y'], xl)
+            plt.scatter(cords['x'], cords['y'])
+            plt.plot(xl, yl, label=label)
+        self._setting_and_show("Промежуточные значения для данных, полученные посредством построения полинома Лагранжа")
+
+    def draw_piecewise_linear_interpolation(self):
+        filtered_data = filter_data(self.data)
+        for label, cords in filtered_data.items():
+            yl = [Interpolation.piecewise_linear_interpolation(cords['x'], cords['y'], value) for value in cords['x']]
+            plt.scatter(cords['x'], cords['y'])
+            plt.plot(cords['x'], yl, label=label)
+        self._setting_and_show(
+            "Промежуточные значения для данных, полученные с использованием кусочно-линейного интерполирования")
+
+    def draw_piecewise_parabolic_interpolation(self):
+        filtered_data = filter_data(self.data)
+        for label, cords in filtered_data.items():
+            x_value = np.linspace(min(cords['x']), max(cords['x']), 1000)
+            y_value = [Interpolation.piecewise_parabolic_interpolation(cords['x'], cords['y'], x) for x in x_value]
+            plt.plot(x_value, y_value)
+            plt.scatter(cords['x'], cords['y'], label=label)
+        self._setting_and_show(
+            "Промежуточные значения для данных, полученные с использованием кусочно-параболического интерполирования")
+
+    def draw_spline_interpolation(self):
+        filtered_data = filter_data(self.data)
+        for label, cords in filtered_data.items():
+            xl = np.linspace(min(cords['x']), max(cords['x']))
+            tck = interpolate.splrep(cords['x'], cords['y'], s=0)
+            yl = interpolate.splev(xl, tck, der=0)
+            plt.scatter(cords['x'], cords['y'])
+            plt.plot(xl, yl, label=label)
+        self._setting_and_show("Промежуточные значения для данных, полученные с использованием сплайн интерполирования")
+
+    def plot_approximation(self):
+        filtered_data = filter_data(self.data)
+        power = int(input("Степень аппроксимирующего полинома: "))
+        for label, cords in filtered_data.items():
+            approximation(cords['x'], cords['y'], power)
+            plt.scatter(cords['x'], cords['y'], label=label)
+        self._setting_and_show("Задача аппроксимации")
